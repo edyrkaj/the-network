4b3a9c27cfc255fc242be5169ff1e30197004cdc
hennazus <hennazus@34079d14-e8fa-91c8-7b04-95f4a15cac94>
2011-03-01 08:27:55 +0000

    Workey api hämtas med curl

---
+++ trunk/script/workeycurl.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+if(isset($_GET['search'])){
+	$keywords = $_GET['search'];
+}
+else{
+	$keywords = '';
+}
+if(isset($_GET['location'])){
+	$region = $_GET['location'];
+}
+else{
+	$region = 0;
+}
+if(isset($_GET['page'])){
+	$page = $_GET['page'];
+}
+else{
+	$page = 1;
+}
+
+//xml-stylesheet type="text/xsl" href="workey.xsl"
+// create a new cURL resource
+header("Content-Type:	text/xml");
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, "http://api.workey.se/v1/?function=search&region=".$region."&keywords=".$keywords."&page=".$page);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+// grab URL and pass it to the browser
+$curl = curl_exec($ch);
+// close cURL resource, and free up system resources
+curl_close($ch);
+
+
+?>
